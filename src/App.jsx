95cf9e85bd80fbe20d1f11ade35bc4cc879e977c
root <root@LAPTOP-7B54OGNU>
2024-10-16 20:47:21 +0530

    updated App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,19 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { BrowserRouter, Routes , Route} from 'react-router-dom'
+import { Homepage } from './pages/Homepage'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
+      <BrowserRouter >
+      <Routes>
+        {/* all paths  */}
+        <Route path="/" element={<Homepage />}></Route>
+
+      </Routes>
+      </BrowserRouter>
     </>
   )
 }
