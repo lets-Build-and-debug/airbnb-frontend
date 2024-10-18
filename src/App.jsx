c8025c8c1e43f942a04be6c6c5b404928929c7de
root <root@LAPTOP-7B54OGNU>
2024-10-18 23:06:49 +0530

    added few components

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
 import { useState } from 'react'
 import { BrowserRouter, Routes , Route} from 'react-router-dom'
 import { Homepage } from './pages/Homepage'
+import { Stays } from './pages/Stays'
+import { Experiences } from './pages/Experiences'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -11,6 +13,8 @@ function App() {
       <Routes>
         {/* all paths  */}
         <Route path="/" element={<Homepage />}></Route>
+        <Route path="/Stays" element={<Stays />}></Route>
+        <Route path="/Experiences" element={<Experiences />}></Route>
 
       </Routes>
       </BrowserRouter>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,21 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [],
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily : {
+        sans : [ 'Harmonia Sans', 'sans-serif'], // Add Harmonia Sans as default sans font
+      }
+    },
+    screens : {
+      sm: '743px',
+      md: '949px',
+      lg: '1127px',
+      xl: '1440px'
+    }
   },
   plugins: [],
 }
-
